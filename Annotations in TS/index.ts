94bc1d939fdb822b1c50fd2ab5f5b406b5bbfa64
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-10-01 10:27:52 +0530

    edited index.ts

--- Annotations in TS/index.ts
+++ Annotations in TS/index.ts
@@ -1,9 +1,12 @@
-type Person = {
-    name: string
-};
+/* Annotations are used to specify the data type of a variable, parameter, function return value and other type of values.
+Help developers to catch errors early in development by allowing them to specify what type of values can be assigned to a given variable.*/
 
-const person: Person = {
-    name: "John",
-};
+// Strings
+let myName: string = "Ashwin";
+console.log(myName);  // Output => Ashwin
 
-console.log(person.name)
+let num : number = 8;
+console.log(num)  //Ouput => 8
+
+// If we set 'num = true' => it will give an error that it is not assignable.
+
